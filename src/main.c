ec17a37851987b409de0a577f5f09230ea09f526
i99dev <obaid.altuniji@gmail.com>
2021-11-22 10:43:28 +0000

    update init window

--- src/main.c
+++ src/main.c
@@ -12,34 +12,33 @@
 
 #include "../include/fdf.h"
 
-/*
-init the window and the hook for the keyboard and mouse,
-then call the function that will draw the map
-*/
-void	ft_fdf_init(t_fdf fdf)
+/**
+ * @brief initialise window for fdf
+ * 
+ * @param filepath path of the file 
+ */
+void	ft_fdf_init(char *filepath)
 {
+	t_fdf	fdf;
+
 	if (window_init(fdf) == false)
 		ft_putstr_fd(ERROR"error window init\n"END, 1);
 }
 
-/*
-entry point for application
-you need two arguments to run the program :
-1. the path to the file
-2. the name of the file
-example : ./fdf maps/map.fdf
-if there is no file in the maps folder, 
-you will get an error message and the program will exit.
-*/
+/**
+ * @brief entry point for application
+ * 
+ * @param argv 
+ * @param arg 
+ * @return int 
+ */
 
 int	main(int argv, char **arg)
 {
-	t_fdf	fdf;
-
 	if (argv == 2)
 	{
 		ft_putstr_fd(SUCCESS"Your app will work.\n", 1);
-		ft_fdf_init(fdf);
+		ft_fdf_init(arg[1]);
 	}
 	else if (argv == 1)
 	{
