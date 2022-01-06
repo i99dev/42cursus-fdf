2c75dd3f5ff080a3f9840ee1c9370f63e7226e22
i99dev <obaid.altuniji@gmail.com>
2022-01-06 04:16:40 +0000

    update now

--- src/drawing.c
+++ src/drawing.c
@@ -6,7 +6,7 @@
 /*   By: oal-tena <oal-tena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 13:26:33 by oal-tena          #+#    #+#             */
-/*   Updated: 2022/01/05 14:28:11 by oal-tena         ###   ########.fr       */
+/*   Updated: 2022/01/06 04:16:29 by oal-tena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,25 @@ void	put_pixel(t_fdf *fdf, int x, int y, int color)
 	fdf->img_data[i] = color;
 	fdf->img_data[i + 1] = color >> 8;
 	fdf->img_data[i + 2] = color >> 16;
+}
+
+//https://aurelienbrabant.fr/blog/pixel-drawing-with-the-minilibx
+
+void	draw_line(t_fdf *fdf, int x, int y, int color)
+{
+	char    *pixel;
+	int		i;
+
+	i = fdf->bpp - 8;
+    pixel = fdf->img_data + (y * fdf->size_line + x * (fdf->bpp / 32));
+	while (i >= 0)
+	{
+		if (fdf->endian != 0)
+			*pixel++ = (color >> i) & 0xFF;
+		else
+			*pixel++ = (color >> (fdf->bpp - 8 - i)) & 0xFF;
+		i -= 8;
+	}
 }
 
 void	put_map_image(t_fdf *fdf)
@@ -42,8 +61,12 @@ void	put_map_image(t_fdf *fdf)
 				put_pixel(fdf, j, i, 0x042FFF);
 			else if (fdf->map[i][j] > 0)
 				put_pixel(fdf, j, i, 0xFF0000);
+			draw_line(fdf, i, j, 0xFFFFFF);
 			j++;
+			draw_line(fdf, i, j, 0xFFFFFF);
 		}
+		printf("\n");
+
 		i++;
 	}
 }
