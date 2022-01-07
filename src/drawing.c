37f6a3dd03c86ef0f16935b6e6290f9be6c7a692
i99dev <obaid.altuniji@gmail.com>
2022-01-07 12:11:44 +0000

    update

--- src/drawing.c
+++ src/drawing.c
@@ -6,7 +6,7 @@
 /*   By: oal-tena <oal-tena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 13:26:33 by oal-tena          #+#    #+#             */
-/*   Updated: 2022/01/07 07:16:06 by oal-tena         ###   ########.fr       */
+/*   Updated: 2022/01/07 11:39:39 by oal-tena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,8 +65,8 @@ void	put_map_image(t_fdf *fdf)
 	int		y;
 	int		x;
 
-	fdf->map_dx = (fdf->img_width * 0.8) / fdf->map_width;
-	fdf->map_dy = (fdf->img_height * 0.8) / fdf->map_height;
+	fdf->map_dx = (fdf->img_width * 1) / fdf->map_width;
+	fdf->map_dy = (fdf->img_height * 1) / fdf->map_height;
 	y = 0;
 	while (y < fdf->map_height)
 	{
@@ -75,44 +75,11 @@ void	put_map_image(t_fdf *fdf)
 		{
 			if (fdf->map[y][x] == 0)
 				pix_put_img(fdf, x, y, 0xFFFFFF);
-			else if (fdf->map[y][x] == 1)
+			else if (fdf->map[y][x] > 0)
 				pix_put_img(fdf, x, y, 0xFF0000);
-			else if (fdf->map[y][x] == 2)
+			else if (fdf->map[y][x] < 2)
 				pix_put_img(fdf, x, y, 0x00FF00);
-			else if (fdf->map[y][x] == 3)
-				pix_put_img(fdf, x, y, 0x0000FF);
-			else if (fdf->map[y][x] == 4)
-				pix_put_img(fdf, x, y, 0xFFFF00);
-			else if (fdf->map[y][x] == 5)
-				pix_put_img(fdf, x, y, 0x00FFFF);
-			else if (fdf->map[y][x] == 6)
-				pix_put_img(fdf, x, y, 0xFF00FF);
-			else if (fdf->map[y][x] == 7)
-				pix_put_img(fdf, x, y, 0x00FFFF);
-			else if (fdf->map[y][x] == 8)
-				pix_put_img(fdf, x, y, 0xFFFFFF);
-			else if (fdf->map[y][x] == 9)
-				pix_put_img(fdf, x, y, 0xFFFFFF);
-			else if (fdf->map[y][x] == 10)
-				pix_put_img(fdf, x, y, 0xFFFFFF);
-			else if (fdf->map[y][x] == 11)
-				pix_put_img(fdf, x, y, 0xFFFFFF);
-			else if (fdf->map[y][x] == 12)
-				pix_put_img(fdf, x, y, 0xFFFFFF);
-			else if (fdf->map[y][x] == 13)
-				pix_put_img(fdf, x, y, 0xFFFFFF);
-			else if (fdf->map[y][x] == 14)
-				pix_put_img(fdf, x, y, 0xFFFFFF);
-			else if (fdf->map[y][x] == 15)
-				pix_put_img(fdf, x, y, 0xFFFFFF);
-			else if (fdf->map[y][x] == 16)
-				pix_put_img(fdf, x, y, 0xFFFFFF);
-			else if (fdf->map[y][x] == 17)
-				pix_put_img(fdf, x, y, 0xFFFFFF);
-			else if (fdf->map[y][x] == 18)
-				pix_put_img(fdf, x, y, 0xFFFFFF);
-			else if (fdf->map[y][x] == 19)
-				pix_put_img(fdf, x, y, 0xFFFFFF);
+
 			x++;
 		}
 		y++;
